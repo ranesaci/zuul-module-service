2c2e4d331eab37ef3d240bbcb4350fb5d3bec42d
sachin <ranesaci@gmail.com>
2017-12-29 17:43:36 +0530

    changes

--- src/main/java/com/sachin4java/zuulmoduleservice/ZuulModuleServiceApplication.java
+++ src/main/java/com/sachin4java/zuulmoduleservice/ZuulModuleServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 public class ZuulModuleServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(ZuulModuleServiceApplication.class, args);
 	}
 }
